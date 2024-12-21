4a8e19e7e9c66717d589bf77392c7b55d3c680b5
rodrigobesmeraldino <rodrigo.esmeraldino@instivo.com.br>
2024-12-20 21:27:17 -0300

    add: stepper

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "~/styles/globals.css";
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
 import { TRPCReactProvider } from "~/trpc/react";
+import { Toaster } from "~/components/ui/sonner";
 
 export const metadata: Metadata = {
   title: "Create T3 App",
@@ -17,6 +18,7 @@ export default function RootLayout({
     <html lang="en" className={`${GeistSans.variable}`} >
       <body>
         <TRPCReactProvider>{children}</TRPCReactProvider>
+        <Toaster richColors/>
       </body>
     </html>
   );
