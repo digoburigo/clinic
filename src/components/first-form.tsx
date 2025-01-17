30fce17629f06e22c73a6d884030644149a7352c
rodrigobesmeraldino <rodrigo.esmeraldino@instivo.com.br>
2025-01-17 12:38:37 -0300

    build(deps): update pkgs

--- src/components/first-form.tsx
+++ src/components/first-form.tsx
@@ -1,161 +1,205 @@
+"use client";
+
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Input } from "~/components/ui/input";
-import { useForm, Form } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { toast } from "sonner";
 import { z } from "zod";
-import { FormField, FormItem, FormLabel, FormControl, FormMessage } from "./ui/form";
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+} from "./ui/form";
 import { useStepper } from "./ui/stepper";
-import { StepperFormActions } from "./custom-stepper";
+import { Button } from "./ui/button";
 
 const FirstFormSchema = z.object({
-    username: z.string().min(2, {
-      message: "Username must be at least 2 characters.",
-    }),
-    cpf: z.string().min(11, {
-      message: "CPF must be at least 11 characters.",
-    }),
-    celular: z.string().min(10, {
-      message: "Celular must be at least 10 characters.",
-    }),
-    email: z.string().email({
-      message: "Invalid email address.",
-    }),
-    sexo: z.string().nonempty({
-      message: "Sexo is required.",
-    }),
-    responsavel: z.string().optional(),
-    nacionalidade: z.string().nonempty({
-      message: "Nacionalidade is required.",
-    }),
-    raca_cor: z.string().nonempty({
-      message: "Raça / Cor is required.",
-    }),
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+  cpf: z.string().min(11, {
+    message: "CPF must be at least 11 characters.",
+  }),
+  celular: z.string().min(10, {
+    message: "Celular must be at least 10 characters.",
+  }),
+  email: z.string().email({
+    message: "Invalid email address.",
+  }),
+  sexo: z.string().nonempty({
+    message: "Sexo is required.",
+  }),
+  responsavel: z.string().optional(),
+  nacionalidade: z.string().nonempty({
+    message: "Nacionalidade is required.",
+  }),
+  raca_cor: z.string().nonempty({
+    message: "Raça / Cor is required.",
+  }),
+});
+
+export function FirstStepForm() {
+  const { nextStep } = useStepper();
+
+  const form = useForm<z.infer<typeof FirstFormSchema>>({
+    resolver: zodResolver(FirstFormSchema),
+    defaultValues: {
+      username: "",
+    },
   });
-  
-  export function FirstStepForm() {
-    const { nextStep } = useStepper();
-  
-    const form = useForm<z.infer<typeof FirstFormSchema>>({
-      resolver: zodResolver(FirstFormSchema),
-      defaultValues: {
-        username: "",
-      },
-    });
-  
-    function onSubmit(data: z.infer<typeof FirstFormSchema>) {
-      nextStep();
-      toast.success("Parabéns você completou o primeiro passo!");
-    }
-  
-    return (
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-          <FormField
-            control={form.control}
-            name="username"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Nome Completo</FormLabel>
-                <FormControl>
-                  <Input placeholder="Digite seu nome completo" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="cpf"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>CPF</FormLabel>
-                <FormControl>
-                  <Input placeholder="Digite seu CPF" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="celular"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Celular</FormLabel>
-                <FormControl>
-                  <Input placeholder="" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="email"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Email</FormLabel>
-                <FormControl>
-                  <Input placeholder="Digite seu melhor Email" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="sexo"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Sexo</FormLabel>
-                <FormControl>
-                  <Input placeholder="" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="responsavel"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Responsável</FormLabel>
-                <FormControl>
-                  <Input placeholder="" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="nacionalidade"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Nacionalidade</FormLabel>
-                <FormControl>
-                  <Input placeholder="Digite sua nacionalidade" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="raca_cor"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Raça / Cor</FormLabel>
-                <FormControl>
-                  <Input placeholder="" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <StepperFormActions />
-        </form>
-      </Form>
-    );
+
+  function onSubmit(data: z.infer<typeof FirstFormSchema>) {
+    nextStep();
+    toast.success("Parabéns você completou o primeiro passo!");
   }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Nome Completo</FormLabel>
+              <FormControl>
+                <Input placeholder="Digite seu nome completo" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="cpf"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>CPF</FormLabel>
+              <FormControl>
+                <Input placeholder="Digite seu CPF" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="celular"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Celular</FormLabel>
+              <FormControl>
+                <Input placeholder="" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="Digite seu melhor Email" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="sexo"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Sexo</FormLabel>
+              <FormControl>
+                <Input placeholder="" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="responsavel"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Responsável</FormLabel>
+              <FormControl>
+                <Input placeholder="" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="nacionalidade"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Nacionalidade</FormLabel>
+              <FormControl>
+                <Input placeholder="Digite sua nacionalidade" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="raca_cor"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Raça / Cor</FormLabel>
+              <FormControl>
+                <Input placeholder="" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <StepperFormActions />
+      </form>
+    </Form>
+  );
+}
+
+function StepperFormActions() {
+  const {
+    prevStep,
+    resetSteps,
+    isDisabledStep,
+    hasCompletedAllSteps,
+    isLastStep,
+    isOptionalStep,
+  } = useStepper();
+
+  return (
+    <div className="flex w-full justify-end gap-2">
+      {hasCompletedAllSteps ? (
+        <Button size="sm" onClick={resetSteps}>
+          Reset
+        </Button>
+      ) : (
+        <>
+          <Button
+            disabled={isDisabledStep}
+            onClick={prevStep}
+            size="sm"
+            variant="secondary"
+          >
+            Prev
+          </Button>
+          <Button size="sm">
+            {isLastStep ? "Finish" : isOptionalStep ? "Skip" : "Next"}
+          </Button>
+        </>
+      )}
+    </div>
+  );
+}
