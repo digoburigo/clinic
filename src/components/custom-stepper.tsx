e00e247cff8830c3d51b6a8a395a5e126bfda811
rodrigobesmeraldino <rodrigo.esmeraldino@instivo.com.br>
2025-01-19 10:33:29 -0300

    fix import

--- src/components/custom-stepper.tsx
+++ src/components/custom-stepper.tsx
@@ -67,6 +67,40 @@ export default function StepperDemo() {
   );
 }
 
+export function StepperFormActions() {
+  const {
+    prevStep,
+    resetSteps,
+    isDisabledStep,
+    hasCompletedAllSteps,
+    isLastStep,
+    isOptionalStep,
+  } = useStepper();
+
+  return (
+    <div className="flex w-full justify-end gap-2">
+      {hasCompletedAllSteps ? (
+        <Button size="sm" onClick={resetSteps}>
+          Reset
+        </Button>
+      ) : (
+        <>
+          <Button
+            disabled={isDisabledStep}
+            onClick={prevStep}
+            size="sm"
+            variant="secondary"
+          >
+            Prev
+          </Button>
+          <Button size="sm">
+            {isLastStep ? "Finish" : isOptionalStep ? "Skip" : "Next"}
+          </Button>
+        </>
+      )}
+    </div>
+  );
+}
 
 
 function MyStepperFooter() {
--- src/components/first-form.tsx
+++ src/components/first-form.tsx
@@ -2,20 +2,20 @@
 
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Input } from "~/components/ui/input";
-import { useForm} from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { toast } from "sonner";
 import { z } from "zod";
 import { useMaskito } from "@maskito/react";
 import type { MaskitoOptions } from "@maskito/core";
 import {
-    Form,
-    FormControl,
-    FormDescription,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
-  } from "~/components/ui/form";
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "~/components/ui/form";
 import { useStepper } from "./ui/stepper";
 import { StepperFormActions } from "./custom-stepper";
 import {
@@ -56,162 +56,177 @@ const FirstFormSchema = z.object({
     ],
     {
       required_error: "Selecione a raça/cor.",
-    }
+    },
   ),
 });
-  
-  const phoneMaskOptions: MaskitoOptions = {
-    mask: [
-      '+', '5', '5', ' ', 
-      '(', /\d/, /\d/, ')', ' ',
-      /\d/, /\d/, /\d/, /\d/, /\d/, '-',
-      /\d/, /\d/, /\d/, /\d/
-    ],
-  };
-  
-  export function FirstStepForm() {
-    const { nextStep } = useStepper();
-  
-    const form = useForm<z.infer<typeof FirstFormSchema>>({
-      resolver: zodResolver(FirstFormSchema),
-      defaultValues: {
-        username: "",
-      },
-    });
-  
-    function onSubmit(data: z.infer<typeof FirstFormSchema>) {
-      nextStep();
-      toast.success("Parabéns você completou o primeiro passo!");
-    }
-  
-    return (
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-          <FormField
-            control={form.control}
-            name="username"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Nome Completo</FormLabel>
-                <FormControl>
-                  <Input placeholder="Digite seu nome completo" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="cpf"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>CPF</FormLabel>
-                <FormControl>
-                  <Input placeholder="Digite seu CPF" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="celular"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Celular</FormLabel>
-                <FormControl>
-                  <Input placeholder="" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="email"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Email</FormLabel>
-                <FormControl>
-                  <Input placeholder="Digite seu melhor Email" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-  control={form.control}
-  name="sexo"
-  render={({ field }) => (
-    <FormItem>
-      <FormLabel>Sexo</FormLabel>
-      <Select onValueChange={field.onChange} defaultValue={field.value}>
-        <FormControl>
-          <SelectTrigger>
-            <SelectValue placeholder="Selecione o sexo" />
-          </SelectTrigger>
-        </FormControl>
-        <SelectContent>
-          <SelectItem value="masculino">Masculino</SelectItem>
-          <SelectItem value="feminino">Feminino</SelectItem>
-        </SelectContent>
-      </Select>
-      <FormMessage />
-    </FormItem>
-  )}
-/>
-          <FormField
-            control={form.control}
-            name="responsavel"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Responsável</FormLabel>
-                <FormControl>
-                  <Input placeholder="" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="nacionalidade"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Nacionalidade</FormLabel>
-                <FormControl>
-                  <Input placeholder="Digite sua nacionalidade" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-  control={form.control}
-  name="raca_cor"
-  render={({ field }) => (
-    <FormItem>
-      <FormLabel>Raça / Cor</FormLabel>
-      <Select onValueChange={field.onChange} defaultValue={field.value}>
-        <FormControl>
-          <SelectTrigger>
-            <SelectValue placeholder="Selecione a raça/cor" />
-          </SelectTrigger>
-        </FormControl>
-        <SelectContent>
-          <SelectItem value="branco">Branco</SelectItem>
-          <SelectItem value="preto">Preto</SelectItem>
-          <SelectItem value="pardo">Pardo</SelectItem>
-          <SelectItem value="amarelo">Amarelo</SelectItem>
-          <SelectItem value="indigena">Indígena</SelectItem>
-        </SelectContent>
-      </Select>
-      <FormMessage />
-    </FormItem>
-  )}
-  />
-          <StepperFormActions />
-        </form>
-      </Form>
-    );
+
+const phoneMaskOptions: MaskitoOptions = {
+  mask: [
+    "+",
+    "5",
+    "5",
+    " ",
+    "(",
+    /\d/,
+    /\d/,
+    ")",
+    " ",
+    /\d/,
+    /\d/,
+    /\d/,
+    /\d/,
+    /\d/,
+    "-",
+    /\d/,
+    /\d/,
+    /\d/,
+    /\d/,
+  ],
+};
+
+export function FirstStepForm() {
+  const { nextStep } = useStepper();
+
+  const form = useForm<z.infer<typeof FirstFormSchema>>({
+    resolver: zodResolver(FirstFormSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  function onSubmit(data: z.infer<typeof FirstFormSchema>) {
+    nextStep();
+    toast.success("Parabéns você completou o primeiro passo!");
   }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Nome Completo</FormLabel>
+              <FormControl>
+                <Input placeholder="Digite seu nome completo" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="cpf"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>CPF</FormLabel>
+              <FormControl>
+                <Input placeholder="Digite seu CPF" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="celular"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Celular</FormLabel>
+              <FormControl>
+                <Input placeholder="" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="Digite seu melhor Email" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="sexo"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Sexo</FormLabel>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Selecione o sexo" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  <SelectItem value="masculino">Masculino</SelectItem>
+                  <SelectItem value="feminino">Feminino</SelectItem>
+                </SelectContent>
+              </Select>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="responsavel"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Responsável</FormLabel>
+              <FormControl>
+                <Input placeholder="" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="nacionalidade"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Nacionalidade</FormLabel>
+              <FormControl>
+                <Input placeholder="Digite sua nacionalidade" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="raca_cor"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Raça / Cor</FormLabel>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Selecione a raça/cor" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  <SelectItem value="branco">Branco</SelectItem>
+                  <SelectItem value="preto">Preto</SelectItem>
+                  <SelectItem value="pardo">Pardo</SelectItem>
+                  <SelectItem value="amarelo">Amarelo</SelectItem>
+                  <SelectItem value="indigena">Indígena</SelectItem>
+                </SelectContent>
+              </Select>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <StepperFormActions />
+      </form>
+    </Form>
+  );
+}
