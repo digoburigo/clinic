7b52ed567eae7ff8d6b17244fffcdd9af2f7e073
rodrigobesmeraldino <rodrigo.esmeraldino@instivo.com.br>
2025-01-28 23:56:53 -0300

    remove libsql

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,15 +1,15 @@
-import { createClient } from "@libsql/client";
-import { PrismaLibSQL } from "@prisma/adapter-libsql";
+// import { createClient } from "@libsql/client";
+// import { PrismaLibSQL } from "@prisma/adapter-libsql";
 import { PrismaClient } from "@prisma/client";
 
 import { env } from "~/env";
 
-const libsql = createClient({
-  url: `${process.env.TURSO_DATABASE_URL}`,
-  authToken: `${process.env.TURSO_DATABASE_AUTH_TOKEN}`,
-});
+// const libsql = createClient({
+//   url: `${process.env.TURSO_DATABASE_URL}`,
+//   authToken: `${process.env.TURSO_DATABASE_AUTH_TOKEN}`,
+// });
 
-const adapter = new PrismaLibSQL(libsql);
+// const adapter = new PrismaLibSQL(libsql);
 
 const createPrismaClient = () =>
   new PrismaClient({
