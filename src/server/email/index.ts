b58fb3f14d70faee71e091109658aea979b2c326
rodrigobesmeraldino <rodrigobesmeraldino@gmail.com>
2025-02-03 18:43:44 -0300

    feat: fix email domain

--- src/server/email/index.ts
+++ src/server/email/index.ts
@@ -15,43 +15,38 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export async function sendEmail({ emailTemplate, to, subject }: Props) {
   const html = await render(emailTemplate);
 
-  // if (process.env.NODE_ENV === "production") {
-
-  try {
+  if (process.env.NODE_ENV === "production") {
     await resend.emails.send({
       from: "Melomar Clínica <admin@updates.melomarclinica.com.br>",
       to,
       subject,
       html,
     });
-  } catch (error) {
-    console.error(error);
+  } else {
+    const smtpOptions: SMTPTransport.Options = {
+      // remove "service"
+      service: "Gmail",
+      host: process.env.SMTP_HOST || "smtp.mailtrap.io",
+      port: parseInt(process.env.SMTP_PORT || "2525"),
+      // secure: true,
+      secure: false,
+      auth: {
+        user: process.env.SMTP_USER || "user",
+        pass: process.env.SMTP_PASSWORD || "password",
+      },
+    };
+
+    const transporter = nodemailer.createTransport({
+      ...smtpOptions,
+    });
+
+    const options = {
+      from: process.env.SMTP_FROM_EMAIL,
+      to,
+      subject,
+      html,
+    };
+
+    await transporter.sendMail(options);
   }
-  // } else {
-  //   const smtpOptions: SMTPTransport.Options = {
-  //     // remove "service"
-  //     service: "Gmail",
-  //     host: process.env.SMTP_HOST || "smtp.mailtrap.io",
-  //     port: parseInt(process.env.SMTP_PORT || "2525"),
-  //     // secure: true,
-  //     secure: false,
-  //     auth: {
-  //       user: process.env.SMTP_USER || "user",
-  //       pass: process.env.SMTP_PASSWORD || "password",
-  //     },
-  //   };
-
-  //   const transporter = nodemailer.createTransport({
-  //     ...smtpOptions,
-  //   });
-
-  //   const options = {
-  //     from: process.env.SMTP_FROM_EMAIL,
-  //     to,
-  //     subject,
-  //     html,
-  //   };
-
-  //   await transporter.sendMail(options);
-  // }
 }
